d82d70aea5f59b4e8859cdfb239f77b2b2f2eac3
jwksjsjs <jclever108@gmail.com>
2025-06-25 18:06:55 -0300

    Update definicao_dos_pinos.py

--- definicao_dos_pinos.py
+++ definicao_dos_pinos.py
@@ -14,22 +14,22 @@ class Pins:
     STATUS_PIN = 2
      
     @property
-    def pin_sda(self):
+    def pin_sda(self)->int:
         return self.PIN_SDA
 
 
     @property
-    def pin_scl(self):
+    def pin_scl(self)->int:
         return self.PIN_SCL
 
 
     @property
-    def pin_reset(self):
+    def pin_reset(self)->int:
         return self.PIN_RESET
 
 
     @property
-    def led_status(self):
+    def led_status(self)->int:
         return self.STATUS_PIN
 
 
@@ -38,33 +38,33 @@ class Pins:
 
 class PinReset(Pins):
    
-    def __init__(self):
+    def __init__(self)->None:
         super().__init__()
         self._reset_button = Pin(self.pin_reset, Pin.IN, Pin.PULL_UP)
 
 
     @property
-    def reset_button(self):
+    def reset_button(self)->Pin:
         return self._reset_button
        
        
-    def __str__(self):
+    def __str__(self)->str:
         return str(self.reset_button)
                
                
                
 class PinLed(Pins):
        
-    def __init__(self):
+    def __init__(self)->None:
         super().__init__()
         self.ledStatus = Pin(self.led_status, Pin.OUT)      
        
     @property
-    def led(self):
+    def led(self)->Pin:
         return self.ledStatus
  
    
-    def __str__(self):
+    def __str__(self)->str:
         return str(self.led)
  
  
@@ -74,17 +74,17 @@ class BarramentI2C(Pins):
     BARRAMENT_1_I2C = 0
     BARRAMENT_2_I2C = None
    
-    def __init__(self):    
+    def __init__(self)->None:
         super().__init__()
-        self.i2c = I2C(BarramentI2C.BARRAMENT_1_I2C, scl = Pin(self.pin_scl), sda = Pin(self.pin_sda))
+        self._i2c = I2C(BarramentI2C.BARRAMENT_1_I2C, scl = Pin(self.pin_scl), sda = Pin(self.pin_sda))
        
        
     @property
-    def barrament_i2c(self):
-        return self.i2c
+    def barrament_i2c(self)->I2C:
+        return self._i2c
    
    
-    def __str__(self):
+    def __str__(self)->str:
         return str(self.barrament_i2c)
  
      
@@ -99,12 +99,12 @@ class Sockets:
         self.socket_ = self.config_socket()
        
            
-    def config_addr(self):      
+    def config_addr(self)->tuple:
         addr = sck.getaddrinfo(Sockets.IP_CONECTION, Sockets.PIN_HTTP)[0][-1]
         return addr
            
    
-    def config_socket(self):    
+    def config_socket(self)->sck.socket:    
         self.socketAddr = self.config_addr()
         socket_ = sck.socket()
         socket_.bind(self.socketAddr)
@@ -112,12 +112,12 @@ class Sockets:
         return socket_
    
            
-    def socket_accept(self):
+    def socket_accept(self)->tuple:
         conn, addr = self.socket_.accept()  
         return conn, addr
        
        
-    def __str__(self):
+    def __str__(self)->str:
         return str(self.socket_)
 
 
