d993eb90508d5c94ba1e6fbaa6fe2acf9f2a0071
jwksjsjs <jclever108@gmail.com>
2025-06-26 13:22:01 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,31 +6,28 @@ import grafico#funcao em c++
 #Classe de intermédio entre o front e o back, tem que ser equeno, vai so chamar as coisas
 class Main:
    
-    def __init__(self, internetName=None, password=None):
+    def __init__(self, internetName:str=None, password=None)->None:
         self.internetName = internetName
         self.password = password
         self.serverToken = MakerConnection(self.internetName, self.password)
         self.serverSocket = self.serverToken.begin_socket()
        
-    def run_app(self):    
-       #iniciar o servidor em internet.py
-        if 
-        self.severConnection = self.serverToken.begin_connection()
+    def run_app(self)->dict:
+        self.serverConnection = self.serverToken.begin_connection()
         self.connectedIn = self.serverToken.socket_accept()
-        clientSever = {"Id": self.internetName,
-                       "Servidor": self.connecteIn[0]}
+        clientServer = {"Id": self.internetName,
+                       "Servidor": self.connectedIn[0]}
        
         return clientServer
        
        
-    def wifis_around(self):
-       #isso vai ser uma lista ou tupla de um monte de redes, tem que
+    def wifis_around(self)->list[dict]:
         wifisAround = self.serverToken.wifis_scans()
-        wifis = [{"Rede": Wifi[0].decode(), "Potência do sinal": Wifi[3]} for Wifi in wifisAroud]
+        wifis = [{"Rede": _wifi_[0].decode(), "Potência do sinal": _wifi_[3]} for _wifi_ in wifisAround]
         return wifis
        
        
-    def set_sound(self, sound):
+    def set_sound(self, sound:int)->None:
         pass
         #chamar a funcao c++
        
@@ -38,31 +35,25 @@ class Main:
         pass
         #chama o gráfico em c++ e repassa pra web
        
-    def config_autoconnection(self, setSelfConn):
+    def config_autoconnection(self, setSelfConn:bool=False)->None:
         self.serverToken.is_selfconnection(setSelfConn)
         
     
-    def auto_login(self):
-        
+    def auto_login(self)->dict | bool:        
         datasToSelfConn= self.serverToken.make_selfconnection()
-        #usa if pra verificar False, se False retorna pra uma pagina html de erro
-        #ou avisando que nao conseguiu se conectar, tem que ver como diferenciar
         if datasToSelfConn:
             self.internetName, self.password = datasToSelfConn[0], datasToSelfConn[1]
-            self.serverToken = MakerConenction(self.internetName, self.password)
-            self.run_app()
-
+            self.serverToken = MakerConnection(self.internetName, self.password)
+            response = self.run_app()
+            return response
 
         return False
-       #seta a conexão automática, mas tem que impedir que isso ocorra caso a
-       #internet ja estaja conetada
-       #web.py vai chamar isso aqui e fazer o resto com a resposta
        
        
 if __name__ == "__main__":
     try:
-        runner = main()   
+        runner = Main()   
     except:
-        return 0
+        print('Deu merda')
 
 
