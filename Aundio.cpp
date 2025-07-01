f13c831b58a5cf6304f24611b34350b61399762a
jwksjsjs <jclever108@gmail.com>
2025-07-01 15:37:11 -0300

    Create Aundio.cpp

---
+++ Aundio.cpp
@@ -0,0 +1,36 @@
+#include <Wire.h>  // I2C
+
+#define ADAU1701_ADDR 0x34  // Endereço I2C do ADAU1701 (ajuste conforme seu hardware)
+#define REG_VOLUME 0x0001   // Endereço do registrador de volume (exemplo)
+
+// Converte porcentagem 0 a 100 para 32b
+uint32_t percentToQ5_23(float percent) {
+    if (percent < 0) percent = 0;
+    if (percent > 100) percent = 100;
+
+    float decimal = percent / 100.0f;          
+    uint32_t q5_23 = decimal * (1 << 23);
+    return q5_23;
+}
+
+// Envia 4 bytes para o adau
+void writeToADAU(uint16_t reg, uint32_t value) {
+    Wire.beginTransmission(ADAU1701_ADDR);
+
+    // Enviar endereço de 16 bits
+    Wire.write((reg >> 8) & 0xFF);  //byte alto
+    Wire.write(reg & 0xFF);          // byte baixo
+
+    // Enviar valor de 32 bits em 4 bytes
+    Wire.write((value >> 24) & 0xFF);
+    Wire.write((value >> 16) & 0xFF);
+    Wire.write((value >> 8) & 0xFF);
+    Wire.write(value & 0xFF);
+
+    Wire.endTransmission();
+}
+
+void setVolumePercent(float percent) {
+    uint32_t qValue = percentToQ5_23(percent);
+    writeToADAU(REG_VOLUME, qValue);
+}
