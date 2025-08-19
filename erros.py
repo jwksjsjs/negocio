f4d68b4443b788a87ad7d3f7b5c9680e1422e97a
jwksjsjs <jclever108@gmail.com>
2025-08-19 16:50:45 -0300

    Update erros.py

--- erros.py
+++ erros.py
@@ -1,38 +1,26 @@
 
-#Tudo vai ser bem simpleszinho
-#Classe para erros inexperados de tentativa de iniciar o WLAN 
-class WLAMErroException):
-    
-    def __init__(self, mensage:str ="Erro inexperado interno, verifique o estado do microcontrolador e o firmwar"):     
+class ErroWLAN(Exception):
+    def __init__(self, mensage: str = "Erro inesperado interno, verifique o estado do microcontrolador e o firmware"):
         self.erroMensage = mensage
-        
-    def pin_error(self)->bool:
-         print(self.erroMensage)
-         return False
+
+    def pin_error(self) -> bool:
+        print(self.erroMensage)
+        return False
 
 
 class ConnectionError(Exception):
-    def __init__(self, mensage:str = "senha incorreta")->None:
+    def __init__(self, mensage: str = "senha incorreta") -> None:
         self.erroMensage = mensage
 
-    def password_error(self)->bool:
+    def password_error(self) -> bool:
         print(self.erroMensage)
         return False
-        
-        
+
+
 class UnexpectedError(Exception):
-    def __init__(self)->None:
-        self.erroMensage = "Erro inexperado"
+    def __init__(self) -> None:
+        self.erroMensage = "Erro inesperado"
 
-    def error(self)->bool:
+    def error(self) -> bool:
         print(self.erroMensage)
         return False
-    
-
-
-
-
-
-
-
-
